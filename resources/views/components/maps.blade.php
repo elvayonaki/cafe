2ff693109b5b6730d1546239d93ae82468dec799
M Daud Anugerah <daudcanugerah@gmail.com>
2018-07-11 08:13:28 +0700

    revisi maps

--- resources/views/components/maps.blade.php
+++ resources/views/components/maps.blade.php
@@ -1,12 +1,43 @@
-    <div id="map" style="height:400px"></div>
+<div id="col col-12">
+        <div id="map" style="height:400px"></div>
+</div>
+<div id="col col-12">
+    &nbsp;    &nbsp;
+
+</div>
+
+
     <script>
       var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+      var marker;
+      
+      function addMarker(peta,posisi){
+        //   chek marker 
+          if(marker){
+            //   pindahkan marker
+              marker.setPosition(posisi);
+          }else{
+            //   buat marker baru
+            marker = new google.maps.Marker({
+                position : posisi,
+                map : peta
+            })
+          }
+          document.getElementById('lat').value = posisi.lat();
+            document.getElementById('lng').value = posisi.lng();
+      }
+
+      function initMap(){
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8,
+            mapTypeId: 'terrain'
+          });
+        map.addListener('click', function(e) {
+            addMarker(map,e.latLng);
         });
       }
+      // jalankan google maps
     </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC6Gz4LFn1VP4ZV1WckIp0cY4eNF1Mx5Xo&callback=initMap"
     async defer></script>
--- resources/views/createStore.blade.php
+++ resources/views/createStore.blade.php
@@ -1,13 +1,12 @@
-@extends('layouts.user') 
-@section('content')
+@extends('layouts.user') @section('content')
 <div class="col-lg-12">
     <div class="card">
         <div class="card-header">
             <strong>Form Pembuatan Cafe Baru</strong>
         </div>
         <div class="card-body card-block">
-        <form action="{{route('store.store')}}" method="post" enctype="multipart/form-data" id="create-store" class="form-horizontal">
-            {{csrf_field()}}
+            <form action="{{route('store.store')}}" method="post" enctype="multipart/form-data" id="create-store" class="form-horizontal">
+                {{csrf_field()}}
                 <div class="row form-group">
                     <div class="col col-md-3">
                         <label for="text-input" class=" form-control-label">Nama Cafe</label>
@@ -53,19 +52,37 @@
                 </div>
                 @include('components.timeWorks')
                 <div class="col-12">
-                    @include('components.maps');
+                    <div class="col col-md-3">
+                        <label for="text-input" class=" form-control-label">Lokasi Peta</label>
+                    </div>
+                    @include('components.maps')
                 </div>
-            </form>
+                <div class="row form-group">
+                    <div class="col col-md-6">
+                        <input type="text" placeholder="Latitude" id="lat" class="form-control">
+                    </div>
+                    <div class="col col-md-6">
+                        <input type="text" placeholder="Longitude" id="lng" class="form-control">
+                    </div>
+                </div>
+                    <div class="col-12 checkbox">
+                        <label for="checkbox1" class="form-check-label ">
+                            <input type="checkbox" id="checkbox1" name="" value="true" class="form-check-input"> Saya setuju dengan segala ketentuan dan bertanggung jawab atas kebenaran semua informasi yang
+                            telah saya isi.
+                        </label>
+                </div>
         </div>
-        <div class="card-footer">
-            <button type="submit" class="btn btn-primary btn-sm" onclick="event.preventDefault();
-            document.getElementById('create-store').submit()";>
-                <i class="fa fa-dot-circle-o"></i> Submit
-            </button>
-            <button type="reset" class="btn btn-danger btn-sm">
-                <i class="fa fa-ban"></i> Reset
-            </button>
-        </div>
+        </form>
     </div>
+    <div class="card-footer">
+        <button type="submit" class="btn btn-primary btn-sm" onclick="event.preventDefault();
+            document.getElementById('create-store').submit()" ;>
+            <i class="fa fa-dot-circle-o"></i> Submit
+        </button>
+        <button type="reset" class="btn btn-danger btn-sm">
+            <i class="fa fa-ban"></i> Reset
+        </button>
+    </div>
+</div>
 </div>
 @endsection
